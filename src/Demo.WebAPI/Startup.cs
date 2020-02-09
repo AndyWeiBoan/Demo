eb1186e0aea45984ad70c9db4c75e2ea657fb03a
ANDY WEI <andyweiboan@gmail.com>
2020-02-10 00:06:10 +0800

    Install nswag.AspCore nuget package and set up

--- src/Demo.WebAPI/Startup.cs
+++ src/Demo.WebAPI/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Demo.WebAPI
-{
+namespace Demo.WebAPI {
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,6 +18,7 @@ namespace Demo.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOpenApiDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +38,15 @@ namespace Demo.WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseOpenApi(); 
+
+            app.UseSwaggerUi3();
+
+            app.UseReDoc(config =>
+            {
+                config.Path = "/redoc";
             });
         }
     }

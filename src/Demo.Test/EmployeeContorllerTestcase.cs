a20e5e8663c7bd5398cf1083778f06fe998bca0f
ANDY WEI <andyweiboan@gmail.com>
2020-02-10 13:30:56 +0800

    Add EmployeeController unit test

---
+++ src/Demo.Test/EmployeeContorllerTestcase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Demo.Models.Poco;
+using NUnit.Framework;
+
+namespace Demo.Test {
+    internal static class EmployeeContorllerTestcase {
+
+        private const string category = nameof(EmployeeServiceTestcase);
+
+        internal static IEnumerable Get_One_Employee_Testcase {
+            get {
+                var guid = Guid.NewGuid().ToString();
+                Employee item = null;
+                yield return new TestCaseData(guid, item).SetName("取得一位員工的資料回傳NULL").SetCategory(category);
+                yield return new TestCaseData(guid, new Employee {
+                    guid = Guid.NewGuid().ToString(),
+                    LastName = "Andy",
+                    FirstName = "Wei",
+                    Country = "Taiwan",
+                    Email = "abc@gmail.com",
+                    Title = "RD",
+                    Isleave = false,
+                    CreateAt = DateTime.Now
+                }).SetName("取得一位員工的資料").SetCategory(category);
+            }
+        }
+
+        internal static IEnumerable Add_Employee_Testcase {
+            get {
+                yield return new TestCaseData(true, new List<Employee>())
+                    .SetName("新增員工失敗，應該要回傳 200 OK").SetCategory(category);
+                yield return new TestCaseData(true, new List<Employee> {
+                        new Employee {
+                            guid = Guid.NewGuid().ToString(),
+                            LastName = "Andy",
+                            FirstName = "Wei",
+                            Country = "Taiwan",
+                            Email = "abc@gmail.com",
+                            Title = "RD",
+                            Isleave = false,
+                            CreateAt = DateTime.Now
+                        },
+                        new Employee {
+                            guid = Guid.NewGuid().ToString(),
+                            LastName = "Jack",
+                            FirstName = "Chen",
+                            Country = "Taiwan",
+                            Email = "ddc@gmail.com",
+                            Title = "Manager",
+                            Isleave = false,
+                            CreateAt = DateTime.Now
+                        }
+                    }                    
+                ).SetName("新增員工成功，應該要回傳 200 OK").SetCategory(category);
+            }
+        }
+
+        internal static IEnumerable Update_Employee_Testcase {
+            get {
+                var item = new Employee {
+                    guid = Guid.NewGuid().ToString(),
+                    LastName = "Andy",
+                    FirstName = "Wei",
+                    Country = "Taiwan",
+                    Email = "abc@gmail.com",
+                    Title = "RD",
+                    Isleave = false,
+                    CreateAt = DateTime.Now
+                };
+                yield return new TestCaseData(true, item)
+                    .SetName("更新員工失敗，應該要回傳 200 OK").SetCategory(category);
+                yield return new TestCaseData(true, item)
+                    .SetName("更新員工成功，應該要回傳 200 OK").SetCategory(category);
+            }
+        }
+
+    }
+}
---
+++ src/Demo.Test/EmployeeControllerTest.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Demo.Models.Contract;
+using Demo.Models.Poco;
+using Demo.WebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Demo.Test {
+
+    [TestFixture]
+    public class EmployeeControllerTest {
+
+        private ILogger<EmployeeController> _logger;
+        private IEmployeeService _service;
+        private EmployeeController _target;
+
+        [SetUp]
+        public void Setup() {
+            this._logger = Substitute.For<ILoggerStub<EmployeeController>>();
+            this._service = Substitute.For<IEmployeeService>();
+            this._target = new EmployeeController(_logger, _service);
+        }
+
+        [TestCase(TestName ="取得所有員工資料測試成功，應該要回傳 200 OK",Category =nameof(EmployeeControllerTest))]
+        public async Task GetAllEmployeeTest() {
+            // arrange
+            this._service.GetAll().Returns(new List<Employee>());
+
+            // act
+            var resp = await this._target.Get() as ObjectResult;
+
+            // assert
+            Assert.AreEqual(200, resp.StatusCode);
+        }
+
+        [TestCaseSource(typeof(EmployeeContorllerTestcase), 
+            nameof(EmployeeContorllerTestcase.Get_One_Employee_Testcase))]
+        public async Task GetOneEmployeeTest(string guid, Employee res) {
+            // arrange
+            this._service.GetByGuid(guid).Returns(res);
+
+            // act
+            var resp = await this._target.Get(guid) as ObjectResult;
+
+            // assert
+            Assert.AreEqual(200, resp.StatusCode);
+        }
+
+        [TestCase(false,  TestName ="刪除全部員工失敗，應該要回傳200 OK")]
+        [TestCase(true,  TestName ="刪除全部員工成功，應該要回傳200 OK")]
+        public async Task DelAllEmployeeTest(bool Success) {
+            // arrange
+            this._service.DeleteEmployees().Returns(Success);
+
+            // act 
+            var resp = await this._target.Delete() as ObjectResult;
+
+            // assert
+            Assert.AreEqual(200, resp.StatusCode);
+        }
+
+        [TestCaseSource(typeof(EmployeeContorllerTestcase),
+            nameof(EmployeeContorllerTestcase.Add_Employee_Testcase))]
+        public async Task AddEmployeeTest(bool success, IEnumerable<Employee> list) {
+            // arrange
+            this._service.AddNewEmployee(list).Returns(success);
+
+            // act
+            var resp = await this._target.Post(list) as ObjectResult;
+
+            // assert
+            Assert.AreEqual(200, resp.StatusCode);
+        }
+
+        [TestCaseSource(typeof(EmployeeContorllerTestcase),
+            nameof(EmployeeContorllerTestcase.Update_Employee_Testcase))]
+        public async Task UpdateEmployeeInfoTest(bool success, Employee e) {
+            // arrange
+            this._service.UpdataEmployeeInfo(e).Returns(success);
+
+            // act
+            var resp = await this._target.Put(e) as ObjectResult;
+
+            // assert
+            Assert.AreEqual(200, resp.StatusCode);
+        }
+    }
+}
